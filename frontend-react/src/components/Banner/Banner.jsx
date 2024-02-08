18d0fd1e3e6b2ed773fa35d428416dc98d75b3c3
MohamedamaD <mohamed.amad24483@gmail.com>
2024-02-08 18:47:17 +0200

    updata sell-banner

--- frontend-react/src/components/Banner/Banner.jsx
+++ frontend-react/src/components/Banner/Banner.jsx
@@ -1,5 +1,38 @@
 import React from "react";
+import "./Banner.scss";
+import { Button } from "../Button/Button";
+import { Link } from "react-router-dom";
+import { images } from "../../constants";
 
 export const Banner = () => {
-  return <div>Banner</div>;
+  return (
+    <div className="sell-banner">
+      <div className="container">
+        <div className="sell">
+          <h1>بيع عربيتك</h1>
+          <h3> فحص مجاني للعربية، عرض ودفع فوري، بدون أى مصاريف </h3>
+          <Link to="/sell-car">
+            <Button
+              value="بيع عربيتك"
+              className="secondary-bg-color sell-button"
+            />
+          </Link>
+        </div>
+
+        <Link to="/sell-car" className="link-item">
+          <Button
+            value="بيع عربيتك"
+          />
+        </Link>
+
+        <div className="floating-img">
+          <img src={images.CAR_3} alt="car-3" />
+        </div>
+
+        <div className="img-container">
+          <img src={images.CAR_1} alt="car-1" />
+        </div>
+      </div>
+    </div>
+  );
 };
--- frontend-react/src/pages/Home/Home.jsx
+++ frontend-react/src/pages/Home/Home.jsx
@@ -1,53 +1,47 @@
 import React from "react";
 import "./Home.scss";
 import { Header } from "../../containers";
-import carp from "../../assets/phot.png";
-import cars from "../../assets/car2.webp";
-import {Button} from "../../components/Button/Button";
+
+import { Button } from "../../components/Button/Button";
+import { images } from "../../constants";
+import { Banner } from "../../components/Banner/Banner";
 export const Home = () => {
   return (
     <div className="Home">
       <Header />
-      <div className="contine"></div>
+      <div className="container"></div>
       {/* TODO [somaya] : implement latest cars tasks/home */}
 
       {
-      /* TODO [alaa] : implement how site work section tasks/home */
-      <div className="continer1">
-         <div className="start">
-            <h2>كيف تعمل؟</h2>
+        /* TODO [alaa] : implement how site work section tasks/home */
+        <div className="continer1">
+          <div className="start">
+            <h2 className="main-color">كيف تعمل؟</h2>
             <h5> الطريقة الوحيدة الى تشترى بيها عربيه مستعمله</h5>
-         </div>
-         <div className="row">
-              
+          </div>
+          <div className="row">
             <div className="fcol">
-                <h2>اختار عربيتك المفضله والمعتمده من موقعنا</h2>
-                <h5> كل عربيتنا تم اختيارها وفحصها وعليها ضمان شامل لمدة 90 يوم</h5>
-                <h2>اختبر العربيه فى بيتك او فى مركزنا</h2>
-                <h5> تاكد من رضاك عن العربية قبل شرائها</h5>
-                <h2>ادفع كاش او تقسيط وهنوصلهالك لحد باب البيت</h2>
-                <h5>من غير نقاش او سؤال ليك الحق انك ترجع العربية وتسترد فلوسك خلال 7 ايام</h5>
-                <Button value="اعرف المزيد"/>
+              <h2>اختار عربيتك المفضله والمعتمده من موقعنا</h2>
+              <h5>
+                {" "}
+                كل عربيتنا تم اختيارها وفحصها وعليها ضمان شامل لمدة 90 يوم
+              </h5>
+              <h2>اختبر العربيه فى بيتك او فى مركزنا</h2>
+              <h5> تاكد من رضاك عن العربية قبل شرائها</h5>
+              <h2>ادفع كاش او تقسيط وهنوصلهالك لحد باب البيت</h2>
+              <h5>
+                من غير نقاش او سؤال ليك الحق انك ترجع العربية وتسترد فلوسك خلال
+                7 ايام
+              </h5>
+              <Button value="اعرف المزيد" />
             </div>
             <div className="fco2">
-                 <img src={carp} />
+              <img src={images.CAR_2} alt="car-1" />
             </div>
-        
-         </div>
-      </div>
+          </div>
+        </div>
       }
-      {/* TODO [alaa] : implement sell car tasks/home */
-       <div className="continer2">
-       <div className="sell">
-          <h1>بيع عربيتك</h1>
-          <h3> فحص مجاني للعربية، عرض ودفع فوري، بدون أى مصاريف </h3>
-          <Button value="بيع عربيتك"/>
-       </div>
-       <div className="im">
-                 <img src={cars} />
-            </div>
-       </div>
-      }
+      <Banner />
     </div>
   );
 };
