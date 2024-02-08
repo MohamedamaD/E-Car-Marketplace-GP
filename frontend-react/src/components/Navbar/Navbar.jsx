0f6b3efd49941bce775cdb8cfa1ec1f9acc46f75
Alaa Ashraf mo <l.o.fgy1.2.0.9@gmail.com>
2024-02-08 14:18:24 +0200

    sidebar-home update

--- frontend-react/src/components/Navbar/Navbar.jsx
+++ frontend-react/src/components/Navbar/Navbar.jsx
@@ -1,19 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Navbar.scss";
 import { navLinks } from "../../constants";
 import { Link } from "react-router-dom";
 import { BiUser } from "react-icons/bi";
 import { Button } from "../Button/Button";
 import { useSelector } from "react-redux";
-
+import { BiCar} from "react-icons/bi";
+import { FaCarSide } from "react-icons/fa";
+import { CiCircleQuestion } from "react-icons/ci";
+import { GiDreamCatcher } from "react-icons/gi";
+import { BsShop } from "react-icons/bs";
+import { BiLogOutCircle } from "react-icons/bi";
 export const Navbar = () => {
   const { isAuthenticated } = useSelector((state) => state.authentication);
+  const [show,setshow ]=useState(false)
   return (
     <div className="navbar-container container">
+      <i class="ri-menu-line">x</i>
       <Link to="/">
         <div className="brand">
           <img src="" alt="" />
-          <h1>اسم الموقع</h1>
+          <h1>اسم الموقع
+          </h1>
         </div>
       </Link>
       <nav className="nav-bar">
@@ -24,8 +32,40 @@ export const Navbar = () => {
           </Link>
         ))}
       </nav>
-      <nav className="side-bar">
-        {/* TODO [somaya] : here implement sidebar  #note follow layout inside tasks/navbar*/}
+      <nav className="side-bar ">
+        {/* TODO [somaya] : here implement sidebar  #note follow layout inside tasks/navbar*/
+      <div className="comp">
+      <h1> 
+        اسم الموقع
+        <div className="icon-container"><BiLogOutCircle /></div>
+      </h1>
+      <ul>
+        <li>
+          <div className="icon-container"><BiCar /></div>
+          <span>تصفح كل العربيات</span>
+        </li>
+        <li>
+          <div className="icon-container"><FaCarSide /></div>
+          <span>بيع عربيتك</span>
+        </li>
+        <li>
+          <div className="icon-container"><CiCircleQuestion /></div>
+          <span>ليه تخترنا</span>
+        </li>
+        <li>
+          <div className="icon-container"><GiDreamCatcher /></div>
+          <span>اسئلنا على عريية احلامك</span>
+        </li>
+        <li>
+          <div className="icon-container"><BsShop /></div>
+          <span>من نحن</span>
+        </li>
+      </ul>
+            <Button className="btn1" value="بيع عربيتك" />
+            <Button className="btn2" value="اشترى عربية" />
+         </div>
+        
+        }
       </nav>
       <div className="user-profile">
         {isAuthenticated ? (
