0a4dddc189d75d1ef0e7e45e18e57366161e760f
MohamedamaD <mohamed.amad24483@gmail.com>
2024-02-08 17:45:27 +0200

    updata sidebar

--- frontend-react/src/components/Navbar/Navbar.jsx
+++ frontend-react/src/components/Navbar/Navbar.jsx
@@ -2,31 +2,23 @@ import React, { useState } from "react";
 import "./Navbar.scss";
 import { navLinks } from "../../constants";
 import { Link } from "react-router-dom";
-import { BiMenu, BiUser } from "react-icons/bi";
+import { BiMenu, BiUser, BiLogOutCircle } from "react-icons/bi";
 import { Button } from "../Button/Button";
 import { useSelector } from "react-redux";
-import { BiCar} from "react-icons/bi";
-import { FaCarSide } from "react-icons/fa";
-import { CiCircleQuestion } from "react-icons/ci";
-import { GiDreamCatcher } from "react-icons/gi";
-import { BsShop } from "react-icons/bs";
-import { BiLogOutCircle } from "react-icons/bi";
+
 export const Navbar = () => {
   const { isAuthenticated } = useSelector((state) => state.authentication);
-  const [show,setshow ]=useState(false)
   const [sidebarIsActive, setActive] = useState(false);
+
   return (
     <div className="navbar-container container">
-      <i class="ri-menu-line">x</i>
       <Link to="/">
         <div className="brand">
           <img src="" alt="" />
-          <h1>اسم الموقع
-          </h1>
+          <h1>اسم الموقع</h1>
         </div>
       </Link>
       <nav className="nav-bar">
-        {/* TODO [mohamed] : here implement navbar #note follow layout inside tasks/navbar*/}
         {navLinks.map((navLink, i) => (
           <Link key={navLink.title + i} to={`/${navLink.link}`}>
             {navLink.title}
@@ -34,45 +26,39 @@ export const Navbar = () => {
         ))}
       </nav>
 
-      <nav className="side-bar ">
-        {/* TODO [somaya] : here implement sidebar  #note follow layout inside tasks/navbar*/
-      <div className="comp">
-      <h1> 
-        اسم الموقع
-        <div className="icon-container"><BiLogOutCircle /></div>
-      </h1>
-      <ul>
-        <li>
-          <div className="icon-container"><BiCar /></div>
-          <span>تصفح كل العربيات</span>
-        </li>
-        <li>
-          <div className="icon-container"><FaCarSide /></div>
-          <span>بيع عربيتك</span>
-        </li>
-        <li>
-          <div className="icon-container"><CiCircleQuestion /></div>
-          <span>ليه تخترنا</span>
-        </li>
-        <li>
-          <div className="icon-container"><GiDreamCatcher /></div>
-          <span>اسئلنا على عريية احلامك</span>
-        </li>
-        <li>
-          <div className="icon-container"><BsShop /></div>
-          <span>من نحن</span>
-        </li>
-      </ul>
-            <Button className="btn1" value="بيع عربيتك" />
-            <Button className="btn2" value="اشترى عربية" />
-         </div>
-        
-        }
-      </nav>
-
       {sidebarIsActive && (
         <nav className="side-bar">
-          {/* TODO [somaya] : here implement sidebar  #note follow layout inside tasks/navbar*/}
+          <div className="comp">
+            <Link to="/">
+              <div className="brand">
+                <img src="" alt="" />
+                <h1>اسم الموقع</h1>
+              </div>
+            </Link>
+            <div className="icon-container" onClick={() => setActive(false)}>
+              <BiLogOutCircle />
+            </div>
+          </div>
+
+          {navLinks.map((navLink, i) => (
+            <Link
+              key={navLink.title + i}
+              className="sidebar-link"
+              to={`/${navLink.link}`}
+            >
+              {navLink.title}
+              <div className="icon-container">
+                <navLink.Icon />
+              </div>
+            </Link>
+          ))}
+
+          <Link to="/sell-car">
+            <Button className="main-bg-color" value="بيع عربيتك" />
+          </Link>
+          <Link to="/buy-car">
+            <Button className="secondary-bg-color" value="اشترى عربية" />
+          </Link>
         </nav>
       )}
 
